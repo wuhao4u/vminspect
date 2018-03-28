3b1eb1eed2b30a89b05e22efd65fbd90caa989d1
Ubuntu <ubuntu@massopen.cloud>
2018-03-28 20:41:17 +0000

    add celery support

--- vminspect/test_celery_chi/celery.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from celery import Celery
-
-app = Celery('test_celery_chi',
-             broker='amqp://jimmy:jimmy123@localhost/jimmy_vhost',
-             backend='rpc://',
-             include=['test_celery_chi.tasks'])

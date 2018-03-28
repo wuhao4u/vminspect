859b1fe54ddd35c778d05d23926f39a62fb9f9c6
Ubuntu <ubuntu@massopen.cloud>
2018-03-28 21:01:35 +0000

    change

---
+++ vminspect_celery_test/tasks.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from vminspect_celery.celery import app
+import time
+import subprocess
+
+@app.task
+def vminspect_scan(image):
+    print("#########################################################")
+    print("perform security scanning on " + image)
+    subprocess.run(["sudo", "vminspect", 
+        "vulnscan", "-c", 
+        "50", "http://cve.circl.lu/api/search",
+        image]) 

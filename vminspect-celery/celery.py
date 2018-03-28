065ea899e286b441b85f6f6ba8e428ed55b7bfd8
Ubuntu <ubuntu@massopen.cloud>
2018-03-28 20:56:02 +0000

    delete

--- vminspect-celery/celery.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from celery import Celery
-
-app = Celery('vminspect-celery',
-             broker='amqp://jimmy:jimmy123@10.0.0.10/jimmy_vhost',
-             backend='rpc://',
-             include=['vminspect-celery.tasks'])
